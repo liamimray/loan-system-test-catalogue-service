a63810c32cc2ba2074a91cb8c862ed794d413873
Liam Imray <liam@mesma.co.uk>
2025-11-20 13:54:42 +0000

    initial commit

---
+++ Catalogue.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using CatalogueService.Models;
+
+namespace CatalogueService
+{
+
+public class Catalogue
+{
+    private readonly ILogger<Catalogue> _logger;
+
+    public Catalogue(ILogger<Catalogue> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("GetCatalogueItems")]
+    public IActionResult GetCatalogueItems([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        // Create the list of catalogue items
+        var catalogueItems = new List<CatalogueItem>
+        {
+            new CatalogueItem { Id = "0", Name = "MacBook Air M2", Available = true, ImageUrl = "/img/macbook-air-m2.jpg" },
+            new CatalogueItem { Id = "1", Name = "MacBook Pro 14-inch", Available = true, ImageUrl = "/img/macbook-pro-14.avif" },
+            new CatalogueItem { Id = "2", Name = "Dell XPS 13 Laptop", Available = false, ImageUrl = "" },
+            new CatalogueItem { Id = "3", Name = "Dell Latitude 5420 Laptop", Available = true, ImageUrl = "" },
+            new CatalogueItem { Id = "4", Name = "Lenovo ThinkPad X1 Carbon", Available = true, ImageUrl = "" },
+            new CatalogueItem { Id = "5", Name = "Lenovo Yoga 9i Laptop", Available = false, ImageUrl = "" },
+            new CatalogueItem { Id = "6", Name = "iPad Pro 12.9-inch", Available = true, ImageUrl = "" },
+            new CatalogueItem { Id = "7", Name = "iPad Air", Available = true, ImageUrl = "" },
+            new CatalogueItem { Id = "8", Name = "iPad Mini", Available = false, ImageUrl = "" },
+            new CatalogueItem { Id = "9", Name = "Samsung Galaxy Tab S8", Available = true, ImageUrl = "" },
+            new CatalogueItem { Id = "10", Name = "Samsung Galaxy Tab A7", Available = true, ImageUrl = "" },
+            new CatalogueItem { Id = "11", Name = "Microsoft Surface Pro 9", Available = false, ImageUrl = "" },
+            new CatalogueItem { Id = "12", Name = "Microsoft Surface Go 3", Available = true, ImageUrl = "" },
+            new CatalogueItem { Id = "13", Name = "Asus ZenBook 14", Available = true, ImageUrl = "" },
+            new CatalogueItem { Id = "14", Name = "HP Spectre x360", Available = false, ImageUrl = "" },
+            new CatalogueItem { Id = "15", Name = "Acer Swift 5", Available = true, ImageUrl = "" }
+        };
+
+        // Return the catalogue as JSON
+        return new OkObjectResult(catalogueItems);
+    }
+}
+
+}
---
+++ Models/CatalogueItem.cs
@@ -0,0 +1,11 @@
+
+namespace CatalogueService.Models
+{
+    public class CatalogueItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool Available { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
